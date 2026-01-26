d97aa8e0a1ffac50f16dd656c3d83aebd9152a9e
Alexey Samoylov <alexey.samoylov@gmail.com>
2026-01-26 11:03:04 +0600

    feat(cli): init beads with prefix norma if .beads is missing

--- cmd/norma/root.go
+++ cmd/norma/root.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
 
 	"github.com/metalagman/norma/internal/logging"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -29,12 +31,27 @@ func Execute() error {
 	}
 	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
 		logging.Init(debug)
+		if err := initBeads(); err != nil {
+			log.Warn().Err(err).Msg("failed to initialize beads")
+		}
 	}
 	rootCmd.AddCommand(runCmd())
 	rootCmd.AddCommand(runsCmd())
 	rootCmd.AddCommand(taskCmd())
 	rootCmd.AddCommand(uiCmd())
 	return rootCmd.Execute()
+}
+
+func initBeads() error {
+	if _, err := os.Stat(".beads"); err == nil {
+		return nil
+	}
+
+	log.Info().Msg(".beads not found, initializing with prefix 'norma'")
+	cmd := exec.Command("bd", "init", "--prefix", "norma")
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	return cmd.Run()
 }
 
 func initConfig() {
