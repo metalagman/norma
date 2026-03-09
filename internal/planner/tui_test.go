6690cdc03690a97777eec6a8eef6510fc39c9fd0
Alexey Samoylov <alexey.samoylov@gmail.com>
2026-03-09 16:35:44 +0600

    test(planner): add TUI cleanup test

--- internal/planner/tui_test.go
+++ internal/planner/tui_test.go
@@ -1,11 +1,57 @@
 package planner
 
 import (
+	"bytes"
+	"io"
+	"os"
+	"strings"
 	"testing"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"google.golang.org/adk/session"
 )
+
+func TestRunTUI_ExitsCleanly(t *testing.T) {
+	// Capture stdout
+	oldStdout := os.Stdout
+	r, w, _ := os.Pipe()
+	os.Stdout = w
+
+	done := make(chan struct{})
+	var captured string
+	go func() {
+		var buf bytes.Buffer
+		_, _ = io.Copy(&buf, r)
+		captured = buf.String()
+		close(done)
+	}()
+
+	eventChan := make(chan *session.Event)
+	questionChan := make(chan string)
+	responseChan := make(chan string)
+	model, _ := newPlannerModel(eventChan, questionChan, responseChan, nil)
+
+	// Run program in a goroutine so we can send it a quit message
+	p := tea.NewProgram(model, tea.WithOutput(io.Discard), tea.WithInput(strings.NewReader("")))
+	go func() {
+		// Wait a bit for program to start
+		p.Send(tea.Quit())
+	}()
+
+	err := RunTUI(p)
+	if err != nil {
+		t.Errorf("RunTUI() error = %v", err)
+	}
+
+	// Restore stdout
+	_ = w.Close()
+	os.Stdout = oldStdout
+	<-done
+
+	if !strings.HasSuffix(captured, "\n") {
+		t.Errorf("captured output does not end with newline: %q", captured)
+	}
+}
 
 func TestPlannerModel_SendMessageStartsSpinner(t *testing.T) {
 	t.Parallel()
